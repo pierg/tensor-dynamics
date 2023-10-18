707c24b945feb4892ddbbff58a8955bb244fe9fa
Piergiuseppe Mallozzi <pier@Piergiuseppes-MacBook-Pro.local>
2023-10-18 12:45:41 -0700

    asd

--- run.sh
+++ run.sh
@@ -3,8 +3,7 @@
 # Initialize variables with default values
 DATA_DIR="/data"  # Set default value
 CONFIGS=""
-
-# Note: No need to check for mandatory arguments now, since we have a default value
+LAUNCH_BASH=0  # By default, do not launch bash
 
 # Parse arguments
 for arg in "$@"
@@ -16,19 +15,27 @@ do
         CONFIGS=*)
         CONFIGS="${arg#*=}"  # Override if provided
         ;;
+        -c)
+        LAUNCH_BASH=1  # Set flag to launch bash
+        ;;
         *)
         # Ignore unexpected arguments
         ;;
     esac
 done
 
-# No need to check if DATA_DIR is empty, as we're ensuring it's either the default or provided value.
+if [ $LAUNCH_BASH -eq 1 ]; then
+    # If the "-c" argument was passed, launch an interactive bash shell
+    /bin/bash
+    exit  # After bash shell is closed, exit the script
+fi
 
+# Proceed if not launching bash (the rest of your script remains the same)
 # Run the make command depending on whether CONFIGS is provided
 if [ -n "$CONFIGS" ]; then
     # If CONFIGS is provided, include it in the make command
     . .venv/bin/activate && python src/main.py --data_dir=$DATA_DIR $CONFIGS
 else
     # If CONFIGS is not provided, run the make command without it
-    . .venv/bin/activate && python src/main.py --data_dir=$DATA_DIR $CONFIGS
+    . .venv/bin/activate && python src/main.py --data_dir=$DATA_DIR
 fi
