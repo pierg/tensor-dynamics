7b829088163ad4a3157325c80c9a00a5f9ae1481
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2023-10-20 19:53:10 -0700

    mod

--- run.sh
+++ run.sh
@@ -24,23 +24,28 @@ do
     esac
 done
 
+#!/bin/bash
+
+# Assuming LAUNCH_BASH and other variables are set earlier in the script or are passed as environment variables
+
 if [ $LAUNCH_BASH -eq 1 ]; then
     # If the "-c" argument was passed, launch an interactive bash shell
     exec /bin/bash  # 'exec' replaces the current process (the script) with the new command (bash)
 else
-    @if ! pgrep -x "tensorboard" > /dev/null; then \
-		echo "Starting TensorBoard..."; \
-		tensorboard --logdir ./logs & \
-	else \
-		echo "TensorBoard is already running."; \
-	fi
+    if ! pgrep -x "tensorboard" > /dev/null; then
+        echo "Starting TensorBoard..."
+        tensorboard --logdir ./logs &  # This starts tensorboard in the background
+    else
+        echo "TensorBoard is already running."
+    fi
+
     # Proceed if not launching bash (the rest of your script remains the same)
-    # Run the make command depending on whether CONFIGS is provided
+    # Run the python command depending on whether CONFIGS is provided
     if [ -n "$CONFIGS" ]; then
-        # If CONFIGS is provided, include it in the make command
+        # If CONFIGS is provided, include it in the python command
         python src/main.py --data_dir=$DATA_DIR $CONFIGS
     else
-        # If CONFIGS is not provided, run the make command without it
+        # If CONFIGS is not provided, run the python command without it
         python src/main.py --data_dir=$DATA_DIR
     fi
 fi
