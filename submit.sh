3b0f4811d5b01fab80cfaf6fb538fba534402076
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2023-10-23 11:04:56 -0700

    update

--- submit.sh
+++ submit.sh
@@ -2,14 +2,21 @@
 
 #SBATCH -A pc_nnkinetics -p lr6 -q lr_normal -t 0-42:0:0 -N1
 
+
+# Source the secrets file to export GITHUB_TOKEN
 source .secrets
+
+# The 'set +x' command is used to prevent the script from logging the following commands, 
+# protecting the confidentiality of the token.
+set +x 
+
 
 apptainer run \
   --fakeroot \
   --writable-tmpfs \
   --bind /global/scratch/users/edkinigstein/Dataset2/F1:/data \
   --bind /global/home/groups-sw/pc_nnkinetics/results:/app/results \
-  --env GITHUB_RESULTS_REPO \
-  --env GITHUB_TOKEN \
+  --env GITHUB_RESULTS_REPO=$GITHUB_RESULTS_REPO \
+  --env GITHUB_TOKEN=$GITHUB_TOKEN \
   ../neural_networks_latest_v2.sif
 
