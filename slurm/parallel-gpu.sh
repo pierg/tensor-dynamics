d1539f720bbd644cf460b5f2ab93d30b2286dc5f
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2023-10-23 16:23:15 -0700

    update

---
+++ slurm/parallel-gpu.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Define the path to the configurations file
+CONFIG_FILE="../config/configurations.toml"
+
+# Check if the configuration file exists
+if [[ ! -f "$CONFIG_FILE" ]]; then
+    echo "Error: Configuration file does not exist."
+    exit 1
+fi
+
+# Extract configuration identifiers from the TOML file. We're looking for lines that match '[*]'
+# where * is the identifier, then stripping away the square brackets.
+config_ids=$(grep '^\[[^.]*\]$' "$CONFIG_FILE" | tr -d '[]')
+
+# Check if we found any configurations
+if [[ -z "$config_ids" ]]; then
+    echo "Error: No configurations found in the file."
+    exit 1
+fi
+
+# Loop through each configuration identifier and execute the submit command
+while IFS= read -r id; do
+    echo "Submitting configuration $id"
+    # Call the submit script with the configuration ID as a parameter
+    sbatch ./submit-gpu.sh "$id"  # adjusted here
+    # Check the exit status of the submit command and handle any errors
+    if [[ $? -ne 0 ]]; then
+        echo "An error occurred while submitting configuration $id"
+        # Uncomment the next line if you want the script to stop on the first error
+        # exit 1
+    fi
+done <<< "$config_ids"
+
+echo "All configurations have been submitted."
+
+# Exit the script successfully
+exit 0
---
+++ slurm/submit-gpu.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#SBATCH -A pc_nnkinetics -p es1 -q es_normal -t 0-42:0:0 -N 1
+#SBATCH --gres=gpu:1 --ntasks 1 --cpus-per-task=2
+
+# Ensure any error stops the script execution
+set -e 
+
+# Check if configuration id was passed to the script, if not, exit with error.
+if [ -z "$1" ]; then
+    echo "Error: No configuration ID provided."
+    exit 1
+fi
+
+CONFIG_ID=$1
+
+# Source the secrets file to export GITHUB_TOKEN and other variables
+source ../.secrets
+
+# Prevent the script from logging the following commands to protect the confidentiality of the token.
+set +x 
+
+# Ensure required variables are set up.
+if [ -z "$GITHUB_RESULTS_REPO" ] || [ -z "$GITHUB_TOKEN" ]; then
+    echo "Error: Required environment variables GITHUB_RESULTS_REPO or GITHUB_TOKEN are not set."
+    exit 1
+fi
+
+# Run the container with the neural network application, passing in the CONFIG_ID
+apptainer run \
+  --nv \
+  --fakeroot \
+  --writable-tmpfs \
+  --bind /global/scratch/users/edkinigstein/Dataset2/F1:/data \
+  --env GITHUB_RESULTS_REPO="$GITHUB_RESULTS_REPO" \
+  --env GITHUB_TOKEN="$GITHUB_TOKEN" \
+  ../../neural_networks_latest.sif \
+  CONFIGS="$CONFIG_ID"
