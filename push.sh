a65342d2efeaf3729a70ab10524aed3ff3439080
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2023-10-24 14:42:25 -0700

    update

--- push.sh
+++ push.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
 git add --a
+
+
+# Source the secrets file
+source .secrets
+
+# Check if the GITHUB_TOKEN was loaded
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "Error: Failed to load the GITHUB_TOKEN from the .secrets file"
+    exit 1
+fi
+
+# Adding all changes (it should be '.' or '--all', not '--a')
+git add .
+
+# Committing the changes
 git commit -m "update"
-git push
+
+# Pushing the changes
+git push origin main
