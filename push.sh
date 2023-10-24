b3694bcc57b72f2ed949a35213db9eda2450a351
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2023-10-24 14:43:54 -0700

    update

--- push.sh
+++ push.sh
@@ -6,6 +6,13 @@ git add --a
 # Source the secrets file
 source .secrets
 
+git fetch origin
+
+# Merge the changes from the remote branch (e.g., main) into your local branch
+# The strategy-option 'ours' is used to keep local changes in conflict.
+# Note: Replace 'main' with the relevant branch if it's different in your setup.
+git merge -X ours origin/main
+
 # Check if the GITHUB_TOKEN was loaded
 if [ -z "$GITHUB_TOKEN" ]; then
     echo "Error: Failed to load the GITHUB_TOKEN from the .secrets file"
